5c86b82b5619dbb94d500c5cc4f923b75de8926f
RyanAfzal <ryanafzal3@gmail.com>
2023-11-01 13:39:14 +0700

    changed app bar

--- shopping_list/lib/menu.dart
+++ shopping_list/lib/menu.dart
@@ -21,7 +21,11 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text(
           'Shopping List',
+          style: TextStyle(
+            color: Colors.white,
+          ),
         ),
+        backgroundColor: Colors.indigo,
       ),
       body: SingleChildScrollView(
         // Widget wrapper yang dapat discroll
