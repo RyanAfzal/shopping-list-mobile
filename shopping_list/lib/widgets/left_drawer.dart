7d584e3f7223f917483bfe4749e5f2712bd75302
RyanAfzal <ryanafzal3@gmail.com>
2023-11-15 14:16:28 +0700

    Tutorial 8 selesai

--- shopping_list/lib/widgets/left_drawer.dart
+++ shopping_list/lib/widgets/left_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/screens/list_product.dart';
 import 'package:shopping_list/screens/menu.dart';
 
 class LeftDrawer extends StatelessWidget {
@@ -55,6 +56,18 @@ class LeftDrawer extends StatelessWidget {
               setelah halaman ShopFormPage sudah dibuat.
               */
             },
+          ),
+
+          ListTile(
+              leading: const Icon(Icons.shopping_basket),
+              title: const Text('Daftar Produk'),
+              onTap: () {
+                  // Route menu ke halaman produk
+                  Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ProductPage()),
+                  );
+              },
           ),
         ],
       ),
